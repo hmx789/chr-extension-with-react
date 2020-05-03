ed83197d6befcf00f1358fee0456f814cd3f9078
hmx789 <murtazah987@gmail.com>
2020-05-03 15:38:27 -0500

    creating chrome extension

---
+++ dist/popup/precache-manifest.bd95a9a7efc008849cbacbafb0ab56d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ce7896826828c5a8ca4eb5cf0b329d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb3f76a6bcc1fbdfc363",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ea3648c151fc9a9f50c",
+    "url": "/static/js/2.47554412.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.47554412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3f76a6bcc1fbdfc363",
+    "url": "/static/js/main.41093ad4.chunk.js"
+  },
+  {
+    "revision": "0293eae1cdc2fe229161",
+    "url": "/static/js/runtime-main.14fc4c5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
